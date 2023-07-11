94c0eb7172f38904d0ba5de53dc0b970d65d85bc
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-11 13:27:56 +0900

    8강 props

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
-export default function Hello() {
+export default function Hello(props) {
   const [name, setName] = useState("Mike");
+  const [age, setAge] = useState(props.age);
 
   function changeName() {
     const newName = name === "Mike" ? "Jane" : "Mike";
     setName(newName);
+    setAge(age + 1);
   }
 
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>
+        {name}({age}세)
+      </h2>
       <button onClick={changeName}>버튼</button>
     </div>
   );
